abf4ef3b1c23ad4d7e604d12143b7573c54977f2
Maicon Dutra <Maicon_Dutra@Dellteam.com>
2022-05-10 22:33:07 -0300

    teste2

---
+++ Web.GraphQL/Startup.cs
@@ -0,0 +1,39 @@
+using GraphQL;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Web.GraphQL
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDocumentWriter, DocumentWriter>();
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            services.AddTransient<ISchema, GameStoreSchema>();
+            services.AddTransient<GameStoreQuery>();
+
+            services.AddGraphQL(options =>
+            {
+                options.EndPoint = "/graphql";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseGraphQL();
+        }
+    }
+}
